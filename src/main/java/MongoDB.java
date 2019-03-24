51fd17f8b178aa5a462c5a61dd9aa962fa88480d
natalie-todd <natalie.todd15@gmail.com>
2019-03-24 17:41:48 -0600

    issues with codec

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -8,32 +8,39 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 
 import java.text.SimpleDateFormat;
 
+import static java.util.Arrays.asList;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoDB {
     public static void main(String[] args) {
 
-    SimpleDateFormat firstDate = new SimpleDateFormat("2019-03-23");
+        MongoClient mongoClient = MongoClients.create();
 
-    MongoClient mongoClient = MongoClients.create();
-
-    CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings
                 .getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-    MongoDatabase database = mongoClient.getDatabase("dominionDB").withCodecRegistry(pojoCodecRegistry);
+        MongoDatabase database = mongoClient.getDatabase("dominionDB").withCodecRegistry(pojoCodecRegistry);
 
-    MongoCollection<Game> gameCollection = database.getCollection("game", Game.class);
-    MongoCollection<Setup> setupCollection = database.getCollection("setup", Setup.class);
-    MongoCollection<Player> playerCollection = database.getCollection("player", Player.class);
+        MongoCollection<Game> gameCollection = database.getCollection("game", Game.class);
+        MongoCollection<Setup> setupCollection = database.getCollection("setup", Setup.class);
+        MongoCollection<Player> playerCollection = database.getCollection("player", Player.class);
 
-    gameCollection.drop();
-    setupCollection.drop();
-    playerCollection.drop();
+        gameCollection.drop();
+        setupCollection.drop();
+        playerCollection.drop();
 
-        Game test = GameBuilder.gameBuilder().date(null).gameId("bye").scores(null).setup("hi").build();
-        Player testPlayer = new Player("1", "me");
-        System.out.println(testPlayer);
+        Score natScore1 = new Score("1", 33);
+        Score naluScore1 = new Score("2", 23);
+        Setup firstSetup =
+                new Setup("1", asList("Lurker", "Village", "Swindler", "Diplomat", "Throne Room", "Remodel",
+                        "Replace", "Bandit", "Mine", "Harem"), 4);
+
+        Game first = GameBuilder.gameBuilder().date(new SimpleDateFormat("2019-03-23")).gameId("1")
+                .scores(asList(natScore1, naluScore1))
+                .setup("1").winner("Natalie").build();
+
+        gameCollection.insertOne(first);
     }
 
 
